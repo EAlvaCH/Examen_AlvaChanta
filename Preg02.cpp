4ffe387a30c54b13acbb118fd734a37376cb843f
Jose Flores <jfloresxc@gmail.com>
2022-05-11 10:01:29 -0500

    Archivos creados por Jose Flores Chamba

---
+++ Preg02.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+//	Separar un numero en los digitos que lo componen
+	int numero = 0;
+	int numeros [30];
+	
+	cout<<"Ingresa el numero de tu preferencia: ";cin>>numero;
+	
+	int i = 0;
+	while(numero != 0){
+		numeros[i] = numero % 10;
+		numero = numero / 10;
+		i++;
+	}
+	
+	for(int j = i - 1; j >= 0 ;j--){
+		cout<<numeros[j]<<endl;	
+	}
+	
+	return 0;
+}
